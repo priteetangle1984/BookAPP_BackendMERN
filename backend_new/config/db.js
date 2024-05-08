bc8dd8826f2dfb1cecc9af0a9e0c9add38bed6b1
PriteeTangle <140731865+priteetangle1984@users.noreply.github.com>
2024-05-08 11:32:21 -0400

    was on wrong path commiting

--- backend_new/config/db.js
+++ backend_new/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const db =
   "mongodb+srv://Adminpritee:ZLT848zxGBdsd20H@cluster0.ws1jjnt.mongodb.net/MERNFINAL";
-/* Replace <password> with your database password */
+
 
 mongoose.set("strictQuery", true, "useNewUrlParser", true);
 
